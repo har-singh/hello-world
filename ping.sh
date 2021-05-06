eab455a9dc2faf1f59c2761a2cfd531d5632297b
har-singh <51124520+har-singh@users.noreply.github.com>
2021-05-06 13:24:22 +0100

    Update ping.sh

--- ping.sh
+++ ping.sh
@@ -2,8 +2,23 @@
 ##-----HS 30May19-----
 #Script to ping the ip address in the list and print result in one line.
 #useage: ./ping.sh < list.txt
+
+
+# HS 06May21
+# https://stackoverflow.com/questions/18123211/checking-host-availability-by-using-ping-in-bash-scripts
+# https://unix.stackexchange.com/questions/3747/understanding-the-exclamation-mark-in-bash
+# https://stackoverflow.com/questions/5947742/how-to-change-the-output-color-of-echo-in-linux
+
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+#printf "Color ${RED}red${NC} Thanks Stack Overflow\n"
+
 while read line;
 do
-echo -ne $line'\t'
-ping -c1 -w 2 $line | grep -o '[0-9]*% packet loss'
+  if ping -c1 -w 2 $line &> /dev/null
+  then
+    printf "${RED}${line}\tNOK${NC}\n" # exit code 1
+  else
+    printf "${line}\tOK\n" # exit code 0
+  fi
 done
