fe89087cb938a02c3268413d247c687ba7c8aea3
har-singh <51124520+har-singh@users.noreply.github.com>
2021-05-28 15:55:03 +0100

    Update icontrol_master_doc.py

--- icontrol_master_doc.py
+++ icontrol_master_doc.py
@@ -6,7 +6,7 @@
 # the get request is not working when attaching the query with backslash (\) so for now will get everthing thing and extract the information
 # https://likegeeks.com/terminating-python-scripts/
 
-import re, json, csv, datetime, getpass
+import re, json, csv, datetime, getpass, sys
 import requests
 from requests.auth import HTTPBasicAuth
 import urlparse
@@ -56,19 +56,21 @@ def csv_export(data_list, column_keys, hostname):
   #column_keys = ['name', 'destination', 'pool', 'partition']
   #
   #csv_data = [['Virtual Server Name,', 'VS Destination', 'Pool Name', 'Partition','Hostname']] # list initialized with the columns names
-  csv_data = []
+  csv_data = [['Virtual Server Name', 'VS Destination', 'Pool Name', 'Pool Member', 'Node Address', 'Partition', 'Hostname']] # list initialized with the columns names
+  #csv_data = []
   for i in data_list:
     data = [i.get(x) for x in column_keys]
     for j in range(len(data)):
       try:
-        data[j] = re.sub(r'%[0-9]+:', ':', data[j])   # '/Ficus_Prova/10.182.248.95%6:443'. Removes '%6'
-        data[j] = re.sub(r'^/[a-zA-Z_]*/', '', data[j]) # '/Ficus_Prova/10.182.248.95%6:443'. Removes '/Ficus_Prova/'
+        data[j] = re.sub(r'%[0-9]+', '', data[j])   # '/Ficus_Prova/10.182.248.95%6:443'. Removes '%6'
+        data[j] = re.sub(r'^/[0-9a-zA-Z_-]*/', '', data[j]) # '/Ficus_Prova/10.182.248.95%6:443'. Removes '/Ficus_Prova/'
       except:
         pass
     data.append(hostname)
     csv_data.append(data)
   #
-  with open('/shared/tmp/hsingh/bigip_api_export.csv', 'w') as comma_file:
+  file_path_name = '/shared/tmp/hsingh/bigip_' + hostname + '_api_export.csv'
+  with open(file_path_name, 'w') as comma_file:
     wr = csv.writer(comma_file, quoting=csv.QUOTE_ALL)
     wr.writerows(csv_data)
     print('CSV exported.')
@@ -84,18 +86,24 @@ def csv_virtual_export(device, username, password):
   csv_export(virtual_data_list, keys, hostname)
 
 def csv_pool_member_export(device, username, password):
-  keys = ['vs_name', 'vs_destination', 'pool_name', 'member_name', 'address', 'partition', 'hostname']
+  keys = ['vs_name', 'vs_destination', 'pool_name', 'member_name', 'address', 'partition']
   csv_data = [['Virtual Server Name', 'VS Destination', 'Pool Name', 'Pool Member', 'Node Address', 'Partition', 'Hostname']] # list initialized with the columns names
   pool_member_data = []
+  pool_dict = {}
   #
   virtual_data_list = get_request(device, username, password, '/mgmt/tm/ltm/virtual/')
   virtual_data_list = virtual_data_list['items']
+  #
+  pool_data_list = get_request(device, username, password, '/mgmt/tm/ltm/pool/')
+  pool_data_list = pool_data_list['items']
+  for pool in pool_data_list:
+    pool_dict[pool['fullPath']] = pool
+  #
   hostname = get_request(device, username, password, '/mgmt/tm/sys/global-settings?$select=hostname')['hostname']
   #
   for vs in virtual_data_list:
-    if 'poolReference' in vs:
-      pool_path = get_reference_path(vs, 'poolReference')
-      pool_data = get_request(device, username, password, pool_path)
+    if 'pool' in vs:
+      pool_data = pool_dict[vs['pool']]
       if 'membersReference' in pool_data:
         member_path = get_reference_path(pool_data, 'membersReference')
         member_data = get_request(device, username, password, member_path)
@@ -114,11 +122,36 @@ def csv_pool_member_export(device, username, password):
 
 
 if __name__ == "__main__":
+  #
+  pattern_ip = re.compile('^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$') #pattern to match ip address e.g. 10.182.129.65
+  pattern_comma = re.compile('^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3},') # pattern to match ip address with comma e.g. 10.182.129.65
+  device = [sys.argv[0]]
+  #
   # Get access details
-  device = raw_input("Provide BIG-IP Management IP: ")
+  # If no IP are provided the users will be prompted. The list can be separated by spaces or commas
+  if len(sys.argv) == 1:
+    device_ip = raw_input("Provide BIG-IP Management IP: ")
+    device.append(device_ip)
+  elif pattern_comma.match(sys.argv[1]):
+    addr_list = re.split(',', sys.argv[1])
+    for addr in addr_list:
+      device.append(addr)
+  else:
+    device = sys.argv
+  #
+  #
   username = raw_input("Username: ")
   password = getpass.getpass("Password: ")
+
+
+  for index in range(1, len(device)):
+    if pattern_ip.match(device[index]):
+      csv_pool_member_export(device[index], username, password)
+    else:
+      print('Error. ' + device[index] + ' is not an IP address.')
+  
+  #
   #
   # Export to CSV
 #  csv_virtual_export(device, username, password)
-  csv_pool_member_export(device, username, password)
+#  csv_pool_member_export(device, username, password)
